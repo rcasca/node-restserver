4f36c47bcb9d501054b6ac029ee536d42e1c3303
Rafael Castillo <rcastillo@arplatia.com>
2019-02-11 20:24:21 +0100

    Var entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cursonode:cursonode@cluster0-shard-00-00-5wyqs.mongodb.net:27017,cluster0-shard-00-01-5wyqs.mongodb.net:27017,cluster0-shard-00-02-5wyqs.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
